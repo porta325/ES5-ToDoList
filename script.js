cc3f997d039812da505ff46c9542c14b7e56e382
Marian <porta325@yahoo.com>
2017-12-14 13:01:26 +0200

    Set event listeners and init function, add data structure

--- script.js
+++ script.js
@@ -1,20 +1,55 @@
 //TO-DO list controller
 var todoController = (function(){
     
+    var data = {
+        unsolvedTasks: 0,
+        solvedTasks: 0,
+        tasksArr: [],
+        solvedTasksArr: []
+    } 
+    
+    return{
+        
+    }
     
 })();
 
 //UI Controller
 var UIController = (function(){
     
+    return {
+        
+    }
     
 })();
 
 //App controller
 var appController = (function(todoCtrl, UICtrl){
-
+    
+    //Set event listeners
+    var setupEventListeners = function(){
+         document.querySelector(DOM.addButton).addEventListener('click', addItem);
+         document.addEventListener('keypress', function(event){
+            if(event.keycode === 13 || event.which === 13){
+                   addItem();
+            }       
+         });
+    };
+    var addItem =  function(){
+        //Add items   
+    }
+    
+    return{
+        
+        init: function(){
+            //Setup event listeners
+            setupEventListeners();
+            //Display day and date
+        }    
+        
+    }    
     
 })(todoController, UIController);
 
 
-
+appController.init();
