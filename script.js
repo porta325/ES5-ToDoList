6e8439b907a249e3630d5fe3d826d05b4ce36971
Marian <porta325@yahoo.com>
2017-12-14 12:45:10 +0200

    Base files and js file with the basic modules structure

---
+++ script.js
@@ -0,0 +1,20 @@
+//TO-DO list controller
+var todoController = (function(){
+    
+    
+})();
+
+//UI Controller
+var UIController = (function(){
+    
+    
+})();
+
+//App controller
+var appController = (function(todoCtrl, UICtrl){
+
+    
+})(todoController, UIController);
+
+
+
