5c38e00e0c947e40a5e514b7f3e106c9f2a4422d
Marian <porta325@yahoo.com>
2017-12-16 21:30:48 +0200

    Add tasks to the data structure and the UI, delete tasks from data structure and the UI

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 //TO-DO list controller
 var todoController = (function(){
     
+    var Element = function(id, description){
+        this.id = id;
+        this.description = description;
+    };    
+    
     var data = {
         unsolvedTasks: 0,
         solvedTasks: 0,
@@ -9,7 +14,38 @@ var todoController = (function(){
     }; 
     
     return{
+        addData: function(desc){
+            var element, ID;
+            if(data.tasksArr.length === 0){
+                ID = 0;
+            } else if (data.tasksArr.length > 0){
+                ID = data.tasksArr[data.tasksArr.length - 1].id + 1;
+            }
+            element = new Element(ID, desc);
+            data.tasksArr.push(element);
+            return element;
+        },
         
+        noOfTasks: function(){
+            data.unsolvedTasks = data.tasksArr.length;
+            data.solvedTasks = data.solvedTasksArr.length;
+        },
+        
+        getTasks: function(){
+            return {
+                unsolved: data.unsolvedTasks,
+                solved: data.solvedTasks
+            }
+        }, 
+        
+        deleteItem: function(id){
+            var elem, rem;
+            elem = data.tasksArr.map(function(current){
+                return current.id;
+            });
+            rem = elem.indexOf(id);
+            data.tasksArr.splice(rem, 1);
+        }       
     }
     
 })();
@@ -56,7 +92,23 @@ var UIController = (function(){
         displayTasksInUI : function(obj){
               document.querySelector(DOMStrings.tasksNumber).textContent = obj.unsolved;
               document.querySelector(DOMStrings.solvedTasksNumber).textContent = obj.solved;
-        }  
+        },
+        readInputValue: function(){
+            var inputValue;
+            inputValue = document.querySelector(DOMStrings.addInput).value;
+            return inputValue;
+        },
+        updateTasks: function(obj){
+            document.querySelector(DOMStrings.tasksNumber).textContent = obj.unsolved;
+            document.querySelector(DOMStrings.solvedTasksNumber).textContent = obj.solved;
+        },
+        createHTMLElement: function(obj){
+            var html, newHTML;
+            html = '<div class="ttask" id="%id%"><div class="tsk">%desc%</div><div class="del-solved"><div class="del" id="del"><i class="fa fa-trash-o fa-lg"></i></div><div class="solved" id="solved"><i class="fa fa-check-circle fa-lg"></i></div></div></div>';
+            newHTML = html.replace('%id%', obj.id);
+            newHTML = newHTML.replace('%desc%', obj.description);
+            document.querySelector(DOMStrings.toDoTasks).insertAdjacentHTML('beforeend', newHTML);
+        }
     }
     
 })();
@@ -72,13 +124,62 @@ var appController = (function(todoCtrl, UICtrl){
                    addItem();
             }       
          });
+        document.querySelector(DOM.tasks).addEventListener('click', deleteItem);
     };
     //Get DOM Strings
     var DOM = UICtrl.getDOMStrings();
 
+        //Update task number
+    var updateTasks = function(){
+        //Get number of tasks in our data structure
+        todoCtrl.noOfTasks();
+        //Update tasks number in the UI
+        var tasksObj = todoCtrl.getTasks();
+        UICtrl.updateTasks(tasksObj);  
+    };
+    
+    //Add item
     var addItem =  function(){
-        //Add items   
-    }
+        //Check if input field is empty
+        if(document.querySelector(DOM.addInput).value !== ""){
+            document.querySelector(DOM.addInput).classList.remove('red');
+            //Read input field value
+            var inputVal = UICtrl.readInputValue();
+            console.log(inputVal);
+            //Add data to our data struture
+            var addDta = todoCtrl.addData(inputVal);
+            //Create new element
+            var newElement = UICtrl.createHTMLElement(addDta);
+            //Clear field
+            document.querySelector(DOM.addInput).value = '';
+            //Focus back on input fielld
+            document.querySelector(DOM.addInput).focus();
+            //Update Tasks
+            updateTasks();
+        } else {
+            document.querySelector(DOM.addInput).classList.add('red');
+        }
+        
+    };
+    
+    //Delete Item 
+    var deleteItem = function(event){
+        var clickedId, itemId, parent, child;
+        
+        clickedId = event.target.parentNode.id;
+        itemId = parseFloat(event.target.parentNode.parentNode.parentNode.id);
+        
+        if(clickedId === 'del'){
+            //Delete from data structure
+            todoCtrl.deleteItem(itemId);
+            //Delete from the UI
+            parent = event.target.parentNode.parentNode.parentNode.parentNode;
+            child = document.getElementById(itemId);
+            parent.removeChild(child);
+            //Update tasks
+            updateTasks();
+        } 
+    };
     
     return{
         
